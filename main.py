e79b318299f436bdae743324e01cd9e22c32547d
ericw317 <ericwise03@gmail.com>
2024-03-22 01:08:39 -0400

    FileCarving v1.2.0

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from PIL import Image
+import os
 import tkinter
 from tkinter import filedialog
 from tkinter import messagebox
@@ -42,6 +44,14 @@ def carve_jpeg(file):
                             fo = open(output_directory + "/jpeg_file_" + str(file_counter) + ".jpg", "wb")
                             fo.write(jpeg_data)
                             fo.close()
+
+                            try:
+                                img = Image.open(output_directory + "/jpeg_file_" + str(file_counter) + ".jpg")
+                                img.verify()  # Verify the image to check for corruption.
+                                img.close()
+                            except (IOError, SyntaxError) as e:
+                                os.remove(output_directory + "/jpeg_file_" + str(file_counter) + ".jpg")
+
                             file_counter += 1
                             jpeg_data = ''
                             break
@@ -49,9 +59,80 @@ def carve_jpeg(file):
                             jpeg_data.extend(bytearray.fromhex(next_byte.hex()))
     fi.close()
 
+def carve_png(file):
+    file_counter = 1
+    png_data = ''
+    fi = open(file, "rb")
+    # loop to read each byte one at a time
+    while True:
+        byte = fi.read(1)
+        if not byte:
+            break
 
-messagebox.showinfo("File Selection", "Select the file you'd like to carve.")
-file_name = filedialog.askopenfilename()
-messagebox.showinfo("Output Selection", "Select the directory you'd like to output the files to.")
-output_directory = filedialog.askdirectory()
-carve_jpeg(file_name)
+        hex_byte = byte.hex()  # convert byte to hex
+        # check if we are at a png file header
+        if hex_byte == "89":
+            next_bytes = fi.read(7)
+            header = hex_byte + next_bytes.hex()
+            # if we are at png file header, start writing bytes to png_data array
+            if header == "89504e470d0a1a0a":
+                png_data = bytearray.fromhex(header)
+                while True:
+                    byte = fi.read(1)
+                    if not byte:
+                        break
+                    hex_byte = byte.hex()
+
+                    png_data.extend(bytearray.fromhex(hex_byte))
+
+                    # check if we are at the png footer
+                    if hex_byte == "00":
+                        next_byte = fi.read(11)
+                        footer = hex_byte + next_byte.hex()
+                        if not next_byte:
+                            break
+                        if footer == "0000000049454e44ae426082":
+                            png_data.extend(bytearray.fromhex(next_byte.hex()))
+                            fo = open(output_directory + "/png_file_" + str(file_counter) + ".png", "wb")
+                            fo.write(png_data)
+                            fo.close()
+
+                            try:
+                                img = Image.open(output_directory + "/png_file_" + str(file_counter) + ".png")
+                                img.verify()  # Verify the image to check for corruption.
+                                img.close()
+                            except (IOError, SyntaxError) as e:
+                                os.remove(output_directory + "/png_file_" + str(file_counter) + ".png")
+
+                            file_counter += 1
+                            png_data = ''
+                            break
+                        else:
+                            png_data.extend(bytearray.fromhex(next_byte.hex()))
+    fi.close()
+
+
+while True:
+    selection = input("Which type of file would you like to carve?\n"
+                      "1) JPEG\n"
+                      "2) PNG\n"
+                      "3) All of the above\n"
+                      "0) Exit\n")
+    while not selection.isnumeric() or int(selection) < 0 or int(selection) > 3:
+        selection = input("Input must be a number between 0-2. Try again.\n")
+
+    if int(selection) == 0:
+        break
+
+    messagebox.showinfo("File Selection", "Select the file you'd like to carve.")
+    file_name = filedialog.askopenfilename()
+    messagebox.showinfo("Output Selection", "Select the directory you'd like to output the files to.")
+    output_directory = filedialog.askdirectory()
+
+    if int(selection) == 1:
+        carve_jpeg(file_name)
+    elif int(selection) == 2:
+        carve_png(file_name)
+    elif int(selection) == 3:
+        carve_jpeg(file_name)
+        carve_png(file_name)
