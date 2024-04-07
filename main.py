c62c081a179c2619b62dc47cefbdb2400ae96e0f
ericw317 <ericwise03@gmail.com>
2024-04-07 02:13:30 -0400

    FileCarving v1.3.0 Now supports carving zip files and office files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import os
+import zipfile
 import tkinter
 from tkinter import filedialog
 from tkinter import messagebox
@@ -7,6 +8,30 @@ from tkinter import messagebox
 root = tkinter.Tk()
 root.wm_attributes("-topmost", 1)
 root.withdraw()
+
+# checks if a file is a valid zip file or not
+def is_zip(file):
+    try:
+        with zipfile.ZipFile(file, "r") as zip_file:
+            zip_file.testzip()
+        return True
+    except zipfile.BadZipFile:
+        return False
+
+# checks which type of file a compressed file is
+def check_zip_type(file):
+    with zipfile.ZipFile(file, "r") as zip_file:
+        names = zip_file.namelist()
+    if '[Content_Types].xml' in names:
+        if any(name.startswith('word/') for name in names):
+            return "docx"
+        elif any(name.startswith('ppt/') for name in names):
+            return "pptx"
+        elif any(name.startswith('xl/') for name in names):
+            return "xlsx"
+    else:
+        return "zip"
+
 
 def carve_jpeg(file):
     file_counter = 1
@@ -111,12 +136,83 @@ def carve_png(file):
                             png_data.extend(bytearray.fromhex(next_byte.hex()))
     fi.close()
 
+def carve_zip(file):
+    zip_counter = 1
+    docx_counter = 1
+    pptx_counter = 1
+    xlsx_counter = 1
+    zip_data = ''
+    fi = open(file, "rb")
+    # loop to read each byte one at a time
+    while True:
+        byte = fi.read(1)
+        if not byte:
+            break
+
+        hex_byte = byte.hex()  # convert byte to hex
+        # check if we are at a zip file header
+        if hex_byte == "50":
+            next_bytes = fi.read(3)
+            header = hex_byte + next_bytes.hex()
+            # if we are at zip file header, start writing bytes to jpeg_data array
+            if header == "504b0304":
+                zip_data = bytearray.fromhex(header)
+                while True:
+                    byte = fi.read(1)
+                    if not byte:
+                        break
+                    hex_byte = byte.hex()
+
+                    zip_data.extend(bytearray.fromhex(hex_byte))
+
+                    # check if we are at the zip footer
+                    if hex_byte == "50":
+                        next_byte = fi.read(3)
+                        footer = hex_byte + next_byte.hex()
+                        if not next_byte:
+                            break
+                        if footer == "504b0506":
+                            zip_data.extend(bytearray.fromhex(next_byte.hex()))
+                            next_byte = fi.read(18)
+                            zip_data.extend(bytearray.fromhex(next_byte.hex()))
+
+                            # open file to verify it is a valid zip file
+                            file_name = output_directory + "/file"
+                            with open(file_name, "wb") as fo:
+                                fo.write(zip_data)
+
+                            # if it is a valid zip file, check which type it is
+                            if is_zip(file_name):
+                                zip_type = check_zip_type(file_name)
+                                if zip_type == "docx":
+                                    os.rename(file_name, output_directory + "/docx_file_" + str(docx_counter) + ".docx")
+                                    docx_counter += 1
+                                elif zip_type == "pptx":
+                                    os.rename(file_name, output_directory + "/pptx_file_" + str(pptx_counter) + ".pptx")
+                                    pptx_counter += 1
+                                elif zip_type == "xlsx":
+                                    os.rename(file_name, output_directory + "/xlsx_file_" + str(xlsx_counter) + ".xlsx")
+                                    xlsx_counter += 1
+                                else:
+                                    os.rename(file_name, output_directory + "/zip_file_" + str(zip_counter) + ".zip")
+                                    zip_counter += 1
+                            # if it is not a valid zip file, delete it
+                            else:
+                                os.remove(file_name)
+
+                            zip_data = ''
+                            break
+                        else:
+                            zip_data.extend(bytearray.fromhex(next_byte.hex()))
+    fi.close()
+
 
 while True:
     selection = input("Which type of file would you like to carve?\n"
                       "1) JPEG\n"
                       "2) PNG\n"
-                      "3) All of the above\n"
+                      "3) Zip and Modern Office Files\n"
+                      "4) All of the above\n"
                       "0) Exit\n")
     while not selection.isnumeric() or int(selection) < 0 or int(selection) > 3:
         selection = input("Input must be a number between 0-2. Try again.\n")
@@ -134,5 +230,8 @@ while True:
     elif int(selection) == 2:
         carve_png(file_name)
     elif int(selection) == 3:
+        carve_zip(file_name)
+    elif int(selection) == 4:
         carve_jpeg(file_name)
         carve_png(file_name)
+        carve_zip(file_name)
