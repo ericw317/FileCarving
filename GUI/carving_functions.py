02950635eb1a91dfdb17db7894a2570c8277bdb6
ericw317 <51502041+ericw317@users.noreply.github.com>
2024-09-27 22:17:34 -0400

    Add files via upload

---
+++ GUI/carving_functions.py
@@ -0,0 +1,216 @@
+from PIL import Image
+import os
+import zipfile
+
+stop_carving = False
+
+# checks if a file is a valid zip file or not
+def is_zip(file):
+    try:
+        with zipfile.ZipFile(file, "r") as zip_file:
+            zip_file.testzip()
+        return True
+    except:
+        return False
+
+# checks which type of file a compressed file is
+def check_zip_type(file):
+    with zipfile.ZipFile(file, "r") as zip_file:
+        names = zip_file.namelist()
+    if '[Content_Types].xml' in names:
+        if any(name.startswith('word/') for name in names):
+            return "docx"
+        elif any(name.startswith('ppt/') for name in names):
+            return "pptx"
+        elif any(name.startswith('xl/') for name in names):
+            return "xlsx"
+    else:
+        return "zip"
+
+def carve_jpeg(file, output):
+    file_counter = 1
+    jpeg_data = ''
+    fi = open(file, "rb")
+
+    # loop to read each byte one at a time
+    while True:
+        if stop_carving:
+            fi.close()
+            break
+        byte = fi.read(1)
+        if not byte:
+            break
+
+        hex_byte = byte.hex()  # convert byte to hex
+        # check if we are at a jpeg file header
+        if hex_byte == "ff":
+            next_bytes = fi.read(3)
+            header = hex_byte + next_bytes.hex()
+            # if we are at jpeg file header, start writing bytes to jpeg_data array
+            if header == "ffd8ffe0" or header == "ffd8ffe1":
+                jpeg_data = bytearray.fromhex(header)
+                while True:
+                    byte = fi.read(1)
+                    if not byte:
+                        break
+                    hex_byte = byte.hex()
+
+                    jpeg_data.extend(bytearray.fromhex(hex_byte))
+
+                    # check if we are at the jpeg footer
+                    if hex_byte == "ff":
+                        next_byte = fi.read(1)
+                        if not next_byte:
+                            break
+                        if next_byte.hex() == "d9":
+                            jpeg_data.extend(bytearray.fromhex(next_byte.hex()))
+                            fo = open(output + "/jpeg_file_" + str(file_counter) + ".jpg", "wb")
+                            fo.write(jpeg_data)
+                            fo.close()
+
+                            try:
+                                img = Image.open(output + "/jpeg_file_" + str(file_counter) + ".jpg")
+                                img.verify()  # Verify the image to check for corruption.
+                                img.close()
+                            except (IOError, SyntaxError) as e:
+                                os.remove(output + "/jpeg_file_" + str(file_counter) + ".jpg")
+
+                            file_counter += 1
+                            jpeg_data = ''
+                            break
+                        else:
+                            jpeg_data.extend(bytearray.fromhex(next_byte.hex()))
+    fi.close()
+    return 0
+
+def carve_png(file, output):
+    file_counter = 1
+    png_data = ''
+    fi = open(file, "rb")
+    # loop to read each byte one at a time
+    while True:
+        if stop_carving:
+            fi.close()
+            break
+        byte = fi.read(1)
+        if not byte:
+            break
+
+        hex_byte = byte.hex()  # convert byte to hex
+        # check if we are at a png file header
+        if hex_byte == "89":
+            next_bytes = fi.read(7)
+            header = hex_byte + next_bytes.hex()
+            # if we are at png file header, start writing bytes to png_data array
+            if header == "89504e470d0a1a0a":
+                png_data = bytearray.fromhex(header)
+                while True:
+                    byte = fi.read(1)
+                    if not byte:
+                        break
+                    hex_byte = byte.hex()
+
+                    png_data.extend(bytearray.fromhex(hex_byte))
+
+                    # check if we are at the png footer
+                    if hex_byte == "00":
+                        next_byte = fi.read(11)
+                        footer = hex_byte + next_byte.hex()
+                        if not next_byte:
+                            break
+                        if footer == "0000000049454e44ae426082":
+                            png_data.extend(bytearray.fromhex(next_byte.hex()))
+                            fo = open(output + "/png_file_" + str(file_counter) + ".png", "wb")
+                            fo.write(png_data)
+                            fo.close()
+
+                            try:
+                                img = Image.open(output + "/png_file_" + str(file_counter) + ".png")
+                                img.verify()  # Verify the image to check for corruption.
+                                img.close()
+                            except (IOError, SyntaxError) as e:
+                                try:
+                                    img.close()
+                                except:
+                                    print()
+                                os.remove(output + "/png_file_" + str(file_counter) + ".png")
+
+                            file_counter += 1
+                            png_data = ''
+                            break
+                        else:
+                            png_data.extend(bytearray.fromhex(next_byte.hex()))
+    fi.close()
+
+def carve_zip(file, output):
+    zip_counter = 1
+    docx_counter = 1
+    pptx_counter = 1
+    xlsx_counter = 1
+    zip_data = ''
+    fi = open(file, "rb")
+    # loop to read each byte one at a time
+    while True:
+        if stop_carving:
+            fi.close()
+            break
+        byte = fi.read(1)
+        if not byte:
+            break
+
+        hex_byte = byte.hex()  # convert byte to hex
+        # check if we are at a zip file header
+        if hex_byte == "50":
+            next_bytes = fi.read(3)
+            header = hex_byte + next_bytes.hex()
+            # if we are at zip file header, start writing bytes to jpeg_data array
+            if header == "504b0304":
+                zip_data = bytearray.fromhex(header)
+                while True:
+                    byte = fi.read(1)
+                    if not byte:
+                        break
+                    hex_byte = byte.hex()
+
+                    zip_data.extend(bytearray.fromhex(hex_byte))
+
+                    # check if we are at the zip footer
+                    if hex_byte == "50":
+                        next_byte = fi.read(3)
+                        footer = hex_byte + next_byte.hex()
+                        if not next_byte:
+                            break
+                        if footer == "504b0506":
+                            zip_data.extend(bytearray.fromhex(next_byte.hex()))
+                            next_byte = fi.read(18)
+                            zip_data.extend(bytearray.fromhex(next_byte.hex()))
+
+                            # open file to verify it is a valid zip file
+                            file_name = output + "/file"
+                            with open(file_name, "wb") as fo:
+                                fo.write(zip_data)
+
+                            # if it is a valid zip file, check which type it is
+                            if is_zip(file_name):
+                                zip_type = check_zip_type(file_name)
+                                if zip_type == "docx":
+                                    os.rename(file_name, output + "/docx_file_" + str(docx_counter) + ".docx")
+                                    docx_counter += 1
+                                elif zip_type == "pptx":
+                                    os.rename(file_name, output + "/pptx_file_" + str(pptx_counter) + ".pptx")
+                                    pptx_counter += 1
+                                elif zip_type == "xlsx":
+                                    os.rename(file_name, output + "/xlsx_file_" + str(xlsx_counter) + ".xlsx")
+                                    xlsx_counter += 1
+                                else:
+                                    os.rename(file_name, output + "/zip_file_" + str(zip_counter) + ".zip")
+                                    zip_counter += 1
+                            # if it is not a valid zip file, delete it
+                            else:
+                                os.remove(file_name)
+
+                            zip_data = ''
+                            break
+                        else:
+                            zip_data.extend(bytearray.fromhex(next_byte.hex()))
+    fi.close()
---
+++ GUI/main.py
@@ -0,0 +1,136 @@
+import flet as ft
+import carving_functions
+
+def main(page: ft.Page):
+    # functions
+    def get_dir(e: ft.FilePickerResultEvent):
+        if e.path:
+            t_output_directory.value = e.path
+            t_output_directory.update()
+        else:
+            "Cancelled"
+        return 0
+
+    def get_file(e: ft.FilePickerResultEvent):
+        if e.files:
+            t_selected_file.value = e.files[0].path
+            t_selected_file.update()
+        else:
+            "Cancelled"
+        return 0
+
+    def carve_files(file, output_directory):
+        if not error_handling():  # check for any input errors
+            page.window_prevent_close = True  # prevent user from exiting while carving is in progress
+            open_dlg_progress()  # display progress ring
+            page.update()
+            carving_functions.stop_carving = False  # make sure 'stop_carving' is false
+            # run the proper file carving function(s) depending on input
+            if c_jpeg.value:
+                carving_functions.carve_jpeg(file, output_directory)
+            if c_png.value:
+                carving_functions.carve_png(file, output_directory)
+            if c_zip.value:
+                carving_functions.carve_zip(file, output_directory)
+            page.window_prevent_close = False  # allow user to exit after carving is done
+            close_dlg_progress(e=None)  # close progress ring
+            page.update()
+        return 0
+
+    def open_dlg_progress():
+        page.dialog = dlg_progress
+        dlg_progress.open = True
+        page.update()
+
+    def close_dlg_progress(e):
+        carving_functions.stop_carving = True
+        page.window_prevent_close = False
+        dlg_progress.open = False
+        page.update()
+
+    def error_handling():
+        page.dialog = dlg_error
+        if t_selected_file.value == "":
+            dlg_error.content = ft.Text("No file has been selected.")
+            dlg_error.open = True
+            page.update()
+            return True
+        elif t_output_directory.value == "":
+            dlg_error.content = ft.Text("No output directory has been specified.")
+            dlg_error.open = True
+            page.update()
+            return True
+        elif not c_jpeg.value and not c_png.value and not c_zip.value:
+            dlg_error.content = ft.Text("No file type has been selected.")
+            dlg_error.open = True
+            page.update()
+            return True
+        else:
+            return False
+
+    # page settings
+    page.title = "File Carver"
+
+    # dialogues
+    dlg_pick_dir = ft.FilePicker(on_result=get_dir)
+    dlg_pick_file = ft.FilePicker(on_result=get_file)
+    dlg_progress = ft.AlertDialog(title=ft.Text("Carving in Progress"),
+                                  content=ft.ProgressRing(width=25, height=25, stroke_width=2),
+                                  actions=[ft.TextButton("Stop Carving", on_click=close_dlg_progress)],
+                                  actions_alignment=ft.MainAxisAlignment.CENTER,
+                                  modal=True)
+    dlg_error = ft.AlertDialog(title=ft.Text("Error"))
+    page.overlay.append(dlg_pick_dir)
+    page.overlay.append(dlg_pick_file)
+
+    # text fields
+    t_selected_file = ft.TextField(label="File to Carve", read_only=True)
+    t_output_directory = ft.TextField(label="Output directory", read_only=True)
+
+    # buttons
+    b_file_select = ft.ElevatedButton(
+        text="Select File",
+        on_click=lambda _: dlg_pick_file.pick_files()
+    )
+    b_carve = ft.ElevatedButton(
+        text="Carve",
+        on_click=lambda _: carve_files(t_selected_file.value, t_output_directory.value),
+        height=50, width=150
+    )
+    b_output_directory = ft.ElevatedButton(
+        text="Select Directory",
+        on_click=lambda _: dlg_pick_dir.get_directory_path()
+    )
+
+    # checkboxes
+    c_jpeg = ft.Checkbox(label="JPG")
+    c_png = ft.Checkbox(label="PNG")
+    c_zip = ft.Checkbox(label="ZIP and Modern Office Files")
+
+    # page display
+    page.add(
+        ft.Column([
+            ft.Row([
+                t_selected_file,
+                b_file_select,
+            ],
+                alignment=ft.MainAxisAlignment.CENTER
+            ),
+            ft.Container(padding=20),
+            ft.Row([
+                c_jpeg, c_png, c_zip
+            ], alignment=ft.MainAxisAlignment.CENTER),
+            ft.Container(padding=20),
+            ft.Row([
+                t_output_directory, b_output_directory
+            ], alignment=ft.MainAxisAlignment.CENTER),
+            ft.Container(padding=20),
+            ft.Row([
+                b_carve,
+            ], alignment=ft.MainAxisAlignment.CENTER),
+        ], expand=True, alignment=ft.MainAxisAlignment.CENTER)
+    )
+
+
+# run the program
+ft.app(target=main)
