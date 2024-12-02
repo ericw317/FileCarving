f34a62cfbc1fbff0a6603cdd44a8cc30882e1216
Wise Forensics <51502041+ericw317@users.noreply.github.com>
2024-12-01 23:38:07 -0500

    Add files via upload

--- GUI/carving_functions.py
+++ GUI/carving_functions.py
@@ -31,122 +31,124 @@ def carve_jpeg(unallocated_dir, output):
     file_counter = 1
     jpeg_data = ''
 
-    for file in os.listdir(unallocated_dir):
-        file_path = os.path.join(unallocated_dir, file)
-        fi = open(file_path, "rb")
+    for root, _, files in os.walk(unallocated_dir):
+        for file in files:
+            file_path = os.path.join(unallocated_dir, root, file)
+            fi = open(file_path, "rb")
 
-        # loop to read each byte one at a time
-        while True:
-            if stop_carving:
-                fi.close()
-                break
-            byte = fi.read(1)
-            if not byte:
-                break
+            # loop to read each byte one at a time
+            while True:
+                if stop_carving:
+                    fi.close()
+                    break
+                byte = fi.read(1)
+                if not byte:
+                    break
 
-            hex_byte = byte.hex()  # convert byte to hex
-            # check if we are at a jpeg file header
-            if hex_byte == "ff":
-                next_bytes = fi.read(3)
-                header = hex_byte + next_bytes.hex()
-                # if we are at jpeg file header, start writing bytes to jpeg_data array
-                if header == "ffd8ffe0" or header == "ffd8ffe1":
-                    jpeg_data = bytearray.fromhex(header)
-                    while True:
-                        byte = fi.read(1)
-                        if not byte:
-                            break
-                        hex_byte = byte.hex()
-
-                        jpeg_data.extend(bytearray.fromhex(hex_byte))
-
-                        # check if we are at the jpeg footer
-                        if hex_byte == "ff":
-                            next_byte = fi.read(1)
-                            if not next_byte:
+                hex_byte = byte.hex()  # convert byte to hex
+                # check if we are at a jpeg file header
+                if hex_byte == "ff":
+                    next_bytes = fi.read(3)
+                    header = hex_byte + next_bytes.hex()
+                    # if we are at jpeg file header, start writing bytes to jpeg_data array
+                    if header == "ffd8ffe0" or header == "ffd8ffe1":
+                        jpeg_data = bytearray.fromhex(header)
+                        while True:
+                            byte = fi.read(1)
+                            if not byte:
                                 break
-                            if next_byte.hex() == "d9":
-                                jpeg_data.extend(bytearray.fromhex(next_byte.hex()))
-                                fo = open(output + "/jpeg_file_" + str(file_counter) + ".jpg", "wb")
-                                fo.write(jpeg_data)
-                                fo.close()
+                            hex_byte = byte.hex()
 
-                                try:
-                                    img = Image.open(output + "/jpeg_file_" + str(file_counter) + ".jpg")
-                                    img.verify()  # Verify the image to check for corruption.
-                                    img.close()
-                                    file_counter += 1
-                                except (IOError, SyntaxError) as e:
-                                    os.remove(output + "/jpeg_file_" + str(file_counter) + ".jpg")
+                            jpeg_data.extend(bytearray.fromhex(hex_byte))
 
-                                jpeg_data = ''
-                                break
-                            else:
-                                jpeg_data.extend(bytearray.fromhex(next_byte.hex()))
-        fi.close()
+                            # check if we are at the jpeg footer
+                            if hex_byte == "ff":
+                                next_byte = fi.read(1)
+                                if not next_byte:
+                                    break
+                                if next_byte.hex() == "d9":
+                                    jpeg_data.extend(bytearray.fromhex(next_byte.hex()))
+                                    fo = open(output + "/jpeg_file_" + str(file_counter) + ".jpg", "wb")
+                                    fo.write(jpeg_data)
+                                    fo.close()
+
+                                    try:
+                                        img = Image.open(output + "/jpeg_file_" + str(file_counter) + ".jpg")
+                                        img.verify()  # Verify the image to check for corruption.
+                                        img.close()
+                                        file_counter += 1
+                                    except (IOError, SyntaxError) as e:
+                                        os.remove(output + "/jpeg_file_" + str(file_counter) + ".jpg")
+
+                                    jpeg_data = ''
+                                    break
+                                else:
+                                    jpeg_data.extend(bytearray.fromhex(next_byte.hex()))
+            fi.close()
     return 0
 
 def carve_png(unallocated_dir, output):
     file_counter = 1
     png_data = ''
 
-    for file in os.listdir(unallocated_dir):
-        file_path = os.path.join(unallocated_dir, file)
-        fi = open(file_path, "rb")
-        # loop to read each byte one at a time
-        while True:
-            if stop_carving:
-                fi.close()
-                break
-            byte = fi.read(1)
-            if not byte:
-                break
+    for root, _, files in os.walk(unallocated_dir):
+        for file in files:
+            file_path = os.path.join(unallocated_dir, root, file)
+            fi = open(file_path, "rb")
+            # loop to read each byte one at a time
+            while True:
+                if stop_carving:
+                    fi.close()
+                    break
+                byte = fi.read(1)
+                if not byte:
+                    break
 
-            hex_byte = byte.hex()  # convert byte to hex
-            # check if we are at a png file header
-            if hex_byte == "89":
-                next_bytes = fi.read(7)
-                header = hex_byte + next_bytes.hex()
-                # if we are at png file header, start writing bytes to png_data array
-                if header == "89504e470d0a1a0a":
-                    png_data = bytearray.fromhex(header)
-                    while True:
-                        byte = fi.read(1)
-                        if not byte:
-                            break
-                        hex_byte = byte.hex()
-
-                        png_data.extend(bytearray.fromhex(hex_byte))
-
-                        # check if we are at the png footer
-                        if hex_byte == "00":
-                            next_byte = fi.read(11)
-                            footer = hex_byte + next_byte.hex()
-                            if not next_byte:
+                hex_byte = byte.hex()  # convert byte to hex
+                # check if we are at a png file header
+                if hex_byte == "89":
+                    next_bytes = fi.read(7)
+                    header = hex_byte + next_bytes.hex()
+                    # if we are at png file header, start writing bytes to png_data array
+                    if header == "89504e470d0a1a0a":
+                        png_data = bytearray.fromhex(header)
+                        while True:
+                            byte = fi.read(1)
+                            if not byte:
                                 break
-                            if footer == "0000000049454e44ae426082":
-                                png_data.extend(bytearray.fromhex(next_byte.hex()))
-                                fo = open(output + "/png_file_" + str(file_counter) + ".png", "wb")
-                                fo.write(png_data)
-                                fo.close()
+                            hex_byte = byte.hex()
 
-                                try:
-                                    img = Image.open(output + "/png_file_" + str(file_counter) + ".png")
-                                    img.verify()  # Verify the image to check for corruption.
-                                    img.close()
-                                    file_counter += 1
-                                except (IOError, SyntaxError) as e:
+                            png_data.extend(bytearray.fromhex(hex_byte))
+
+                            # check if we are at the png footer
+                            if hex_byte == "00":
+                                next_byte = fi.read(11)
+                                footer = hex_byte + next_byte.hex()
+                                if not next_byte:
+                                    break
+                                if footer == "0000000049454e44ae426082":
+                                    png_data.extend(bytearray.fromhex(next_byte.hex()))
+                                    fo = open(output + "/png_file_" + str(file_counter) + ".png", "wb")
+                                    fo.write(png_data)
+                                    fo.close()
+
                                     try:
+                                        img = Image.open(output + "/png_file_" + str(file_counter) + ".png")
+                                        img.verify()  # Verify the image to check for corruption.
                                         img.close()
-                                    except:
-                                        print()
-                                    os.remove(output + "/png_file_" + str(file_counter) + ".png")
+                                        file_counter += 1
+                                    except (IOError, SyntaxError) as e:
+                                        try:
+                                            img.close()
+                                        except:
+                                            print()
+                                        os.remove(output + "/png_file_" + str(file_counter) + ".png")
 
-                                png_data = ''
-                                break
-                            else:
-                                png_data.extend(bytearray.fromhex(next_byte.hex()))
-        fi.close()
+                                    png_data = ''
+                                    break
+                                else:
+                                    png_data.extend(bytearray.fromhex(next_byte.hex()))
+            fi.close()
 
 def carve_zip(unallocated_dir, output):
     zip_counter = 1
@@ -155,71 +157,72 @@ def carve_zip(unallocated_dir, output):
     xlsx_counter = 1
     zip_data = ''
 
-    for file in os.listdir(unallocated_dir):
-        file_path = os.path.join(unallocated_dir, file)
-        fi = open(file_path, "rb")
-        # loop to read each byte one at a time
-        while True:
-            if stop_carving:
-                fi.close()
-                break
-            byte = fi.read(1)
-            if not byte:
-                break
+    for root, _, files in os.walk(unallocated_dir):
+        for file in files:
+            file_path = os.path.join(unallocated_dir, root, file)
+            fi = open(file_path, "rb")
+            # loop to read each byte one at a time
+            while True:
+                if stop_carving:
+                    fi.close()
+                    break
+                byte = fi.read(1)
+                if not byte:
+                    break
 
-            hex_byte = byte.hex()  # convert byte to hex
-            # check if we are at a zip file header
-            if hex_byte == "50":
-                next_bytes = fi.read(3)
-                header = hex_byte + next_bytes.hex()
-                # if we are at zip file header, start writing bytes to jpeg_data array
-                if header == "504b0304":
-                    zip_data = bytearray.fromhex(header)
-                    while True:
-                        byte = fi.read(1)
-                        if not byte:
-                            break
-                        hex_byte = byte.hex()
-
-                        zip_data.extend(bytearray.fromhex(hex_byte))
-
-                        # check if we are at the zip footer
-                        if hex_byte == "50":
-                            next_byte = fi.read(3)
-                            footer = hex_byte + next_byte.hex()
-                            if not next_byte:
+                hex_byte = byte.hex()  # convert byte to hex
+                # check if we are at a zip file header
+                if hex_byte == "50":
+                    next_bytes = fi.read(3)
+                    header = hex_byte + next_bytes.hex()
+                    # if we are at zip file header, start writing bytes to jpeg_data array
+                    if header == "504b0304":
+                        zip_data = bytearray.fromhex(header)
+                        while True:
+                            byte = fi.read(1)
+                            if not byte:
                                 break
-                            if footer == "504b0506":
-                                zip_data.extend(bytearray.fromhex(next_byte.hex()))
-                                next_byte = fi.read(18)
-                                zip_data.extend(bytearray.fromhex(next_byte.hex()))
+                            hex_byte = byte.hex()
 
-                                # open file to verify it is a valid zip file
-                                file_name = output + "/file"
-                                with open(file_name, "wb") as fo:
-                                    fo.write(zip_data)
+                            zip_data.extend(bytearray.fromhex(hex_byte))
 
-                                # if it is a valid zip file, check which type it is
-                                if is_zip(file_name):
-                                    zip_type = check_zip_type(file_name)
-                                    if zip_type == "docx":
-                                        os.rename(file_name, output + "/docx_file_" + str(docx_counter) + ".docx")
-                                        docx_counter += 1
-                                    elif zip_type == "pptx":
-                                        os.rename(file_name, output + "/pptx_file_" + str(pptx_counter) + ".pptx")
-                                        pptx_counter += 1
-                                    elif zip_type == "xlsx":
-                                        os.rename(file_name, output + "/xlsx_file_" + str(xlsx_counter) + ".xlsx")
-                                        xlsx_counter += 1
+                            # check if we are at the zip footer
+                            if hex_byte == "50":
+                                next_byte = fi.read(3)
+                                footer = hex_byte + next_byte.hex()
+                                if not next_byte:
+                                    break
+                                if footer == "504b0506":
+                                    zip_data.extend(bytearray.fromhex(next_byte.hex()))
+                                    next_byte = fi.read(18)
+                                    zip_data.extend(bytearray.fromhex(next_byte.hex()))
+
+                                    # open file to verify it is a valid zip file
+                                    file_name = output + "/file"
+                                    with open(file_name, "wb") as fo:
+                                        fo.write(zip_data)
+
+                                    # if it is a valid zip file, check which type it is
+                                    if is_zip(file_name):
+                                        zip_type = check_zip_type(file_name)
+                                        if zip_type == "docx":
+                                            os.rename(file_name, output + "/docx_file_" + str(docx_counter) + ".docx")
+                                            docx_counter += 1
+                                        elif zip_type == "pptx":
+                                            os.rename(file_name, output + "/pptx_file_" + str(pptx_counter) + ".pptx")
+                                            pptx_counter += 1
+                                        elif zip_type == "xlsx":
+                                            os.rename(file_name, output + "/xlsx_file_" + str(xlsx_counter) + ".xlsx")
+                                            xlsx_counter += 1
+                                        else:
+                                            os.rename(file_name, output + "/zip_file_" + str(zip_counter) + ".zip")
+                                            zip_counter += 1
+                                    # if it is not a valid zip file, delete it
                                     else:
-                                        os.rename(file_name, output + "/zip_file_" + str(zip_counter) + ".zip")
-                                        zip_counter += 1
-                                # if it is not a valid zip file, delete it
-                                else:
-                                    os.remove(file_name)
+                                        os.remove(file_name)
 
-                                zip_data = ''
-                                break
-                            else:
-                                zip_data.extend(bytearray.fromhex(next_byte.hex()))
-        fi.close()
+                                    zip_data = ''
+                                    break
+                                else:
+                                    zip_data.extend(bytearray.fromhex(next_byte.hex()))
+            fi.close()
